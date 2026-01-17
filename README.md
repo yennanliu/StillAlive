b948368831ddf7d054ff22c6cd9a5dd7ba9db76d
jim <jim@uber.com>
2026-01-17 18:06:05 +0800

    init dart app via android studio

--- README.md
+++
@@ -1,7 +0,0 @@
-# StillAlive
-
-
-
-## Ref
-
-- https://github.com/yennanliu/todo_app_flutter
--- android/app/src/main/kotlin/com/example/still_alive/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.still_alive
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
